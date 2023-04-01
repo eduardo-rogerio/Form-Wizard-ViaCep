9c3931f6f5d3401d95ee2aaad6b83411a67e1a32
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-04-01 00:17:53 -0300

    feat(): configura o filament

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\RegisterForm;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome')->name('home');
+Route::get('/register', RegisterForm::class)->name('register');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-
+const colors = require('tailwindcss/colors')
 module.exports = {
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
             },
         },
     },
@@ -23,6 +29,8 @@ module.exports = {
         './resources/**/*.php',
         './resources/**/*.vue',
         './resources/**/*.twig',
+        './resources/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
     ],
     plugins: [
         require('@tailwindcss/forms'),
