d4b899569348108cf72cf485c19272182cc25612
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-04-02 04:13:58 -0300

    feat(): remove o espaçamento entre o botão e o form

--- resources/views/livewire/register-form-submit.blade.php
+++ resources/views/livewire/register-form-submit.blade.php
@@ -1,4 +1,4 @@
 <x-filament-support::button wire:target="submit" icon="heroicon-o-check" icon-position="after" size="sm" type="submit"
-                            class="mt-4">
+>
     Submit
 </x-filament-support::button>
