2d16be15ba8ee66adcfc448188c119480cddaf21
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-04-01 23:33:18 -0300

    feat(): dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 module.exports = {
-    darkMode: 'class',
+    darkMode: "class",
     theme: {
         extend: {
             fontFamily: {
